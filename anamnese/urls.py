88f7475e198b39b7f60ca1d635deabd071a067e6
Anderson Madruga <66503403+devmadruga@users.noreply.github.com>
2022-10-06 19:16:45 +0000

    Criada primeira View do tipo Create e template. Conseguindo cadastrar Paciente.

--- anamnese/urls.py
+++ anamnese/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.index, name='index')]
+urlpatterns = [
+    #path('', views.index, name='index'),
+    path('cadastrar_paciente/', views.PacienteCreate.as_view(), name='cadastrar_paciente')          
+]
--- anamnese/views.py
+++ anamnese/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Paciente
+from django.views.generic import CreateView
 
-def index(request):
-    return HttpResponse('Oie!')
+
+class PacienteCreate(CreateView):
+    """
+    As CreatView esperam encontrar um template com nome 'model_form.html'.
+    Como o nome do model é Paciente, ela espera 'paciente_form.html'.
+    """
+    model = Paciente
+    fields = '__all__'
