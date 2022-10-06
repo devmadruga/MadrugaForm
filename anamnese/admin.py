d367d021807fd036dec0abaff98b423bd31e55dd
Anderson Madruga <66503403+devmadruga@users.noreply.github.com>
2022-10-06 18:37:48 +0000

    Editei admin.py para conseguir registrar Pacientes pelo admin. Rodei createsuperuse, makemigrations e migrate.

--- anamnese/admin.py
+++ anamnese/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Paciente
+"""
+Para registrar no admin a classe Paciente, precisei
+1. criar o superusuário: python3 manage.py createsuperuser
+2. Rodar: python manage.py makemigrations
+4. Rodar: python manage.py migrate
+"""
 
-# Register your models here.
+admin.site.register(Paciente)
